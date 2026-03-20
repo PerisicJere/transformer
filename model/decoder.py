few fixes in decoder, and mha, encoder backprop

model/decoder.py
@@ -57,15 +57,15 @@ class Decoder:
 
         return third_layer_norm
 
-    def backward(self, gradients: np.ndarray) -> np.ndarray:
+    def backward(self, gradients: np.ndarray) -> tuple[np.ndarray, np.ndarray]:
         # third
         layer_norm_3__backprop_output = self.layer_norm3.backward(gradients=gradients)
         ffnn__backprop_output = self.ffnn.backward_propagation(gradients=layer_norm_3__backprop_output)
         residual_gradient_1  = layer_norm_3__backprop_output + ffnn__backprop_output
 
         # second
-        layer_norm_2__backprop_output = self.layer_norm2.backward(gradients=ffnn__backprop_output)
-        multi_head_attention__backprop_output = self.multi_head_attention.backward(gradients=layer_norm_2__backprop_output)
+        layer_norm_2__backprop_output = self.layer_norm2.backward(gradients=residual_gradient_1)
+        multi_head_attention__backprop_output, d_encoder = self.multi_head_attention.backward(gradients=layer_norm_2__backprop_output, encoder_input=True)
         residual_gradient_2 = multi_head_attention__backprop_output + layer_norm_2__backprop_output
 
         # first
@@ -73,4 +73,4 @@ class Decoder:
         masked_multi_head_attention__backprop_output = self.masked_multi_head_attention.backward(gradients=layer_norm_1__backprop_output)
         residual_gradient_3 = masked_multi_head_attention__backprop_output + layer_norm_1__backprop_output
 
-        return residual_gradient_3
+        return residual_gradient_3, d_encoder

model/encoder_decoder_transformer.py
@@ -25,6 +25,10 @@ class EncoderDecoderTransformer:
 
     def backward(self, probs: np.ndarray, targets: np.ndarray) -> None:
         gradients: np.ndarray = probs - targets
+        d_encoder = np.zeros_like(gradients)
         for decoder in self.decoders:
-            decoder.backward(gradients=gradients)
+            gradients, dx = decoder.backward(gradients=gradients)
+            d_encoder += dx
 
+        for encoder in self.encoders:
+            d_encoder = encoder.backward(gradients=d_encoder)

model/multi_head_attention.py
@@ -28,13 +28,18 @@ class MultiHeadAttention:
         multi_head_attention = self.final_projection(concat)
         return multi_head_attention
 
-    def backward(self, gradients: np.ndarray) -> np.ndarray:
+    def backward(self, gradients: np.ndarray, encoder_input: bool = False) -> tuple[np.ndarray, np.ndarray] | np.ndarray:
         linear_backward = self.final_projection.backward(gradients=gradients)
         heads = np.split(linear_backward, self.num_heads, axis=1)
         dx: np.ndarray = np.zeros_like(heads[0])
+        dQx: np.ndarray = np.zeros_like(heads[0])
         for idx, weights in enumerate(self.head_projections):
             Wq, Wk, Wv, attention = weights
             dQi, dKi, dVi = attention.backward(gradients=heads[idx])
-            dx += Wq.backward(dQi) + Wk.backward(dKi) + Wv.backward(dVi)
+            if encoder_input:
+                dQx += Wq.backward(dQi)
+                dx += Wk.backward(dKi) + Wv.backward(dVi)
+            else:
+                dx += Wq.backward(dQi) + Wk.backward(dKi) + Wv.backward(dVi)
 
-        return dx
+        return (dx, dQx) if encoder_input else dx